5874839cced7019d6cfd5b73971a15ca00cc0077
Uchenna Oko <Uche4newton1981@gmail.com>
2023-06-28 16:14:24 +0000

    Prints an argument based on its type

--- handle_print.c
+++ handle_print.c
@@ -2,7 +2,7 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmat: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @list: List of printed arguments.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
@@ -14,7 +14,7 @@
 int handle_print(const char *fmat, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int n, unknow_len = 0, printed_chars = -1;
 	fmat_t fmat_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +22,11 @@ int handle_print(const char *fmat, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmat_types[i].fmat != '\0'; i++)
-		if (fmat[*ind] == fmat_types[i].fmat)
-			return (fmat_types[i].fn(list, buffer, flags, width, precision, size));
+	for (n = 0; fmat_types[n].fmat != '\0'; n++)
+		if (fmat[*ind] == fmat_types[n].fmat)
+			return (fmat_types[n].fn(list, buffer, flags, width, precision, size));
 
-	if (fmat_types[i].fmat == '\0')
+	if (fmat_types[n].fmat == '\0')
 	{
 		if (fmat[*ind] == '\0')
 			return (-1);
