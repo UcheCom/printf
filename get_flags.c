2efde9cc169be2f74c3553a49b1d4d7230f13a76
Uchenna Oko <Uche4newton1981@gmail.com>
2023-06-29 15:08:08 +0000

    Updated-calculates active flags

--- get_flags.c
+++ get_flags.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_flags -> Active flags calculated
+ * get_flags -> This calculates active flags
  * @format: Formatted string in which to print the arguments
  * @n: parameter to take
  * Return: Flags
@@ -11,16 +11,15 @@ int get_flags(const char *format, int *n)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
+	int m, curr_n;
 	int flags = 0;
-	int m, cur_n;
-
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (cur_n = *n + 1; format[cur_n] != '\0'; cur_n++)
+	for (curr_n = *n + 1; format[curr_n] != '\0'; curr_n++)
 	{
 		for (m = 0; FLAGS_CH[m] != '\0'; m++)
-			if (format[cur_n] == FLAGS_CH[m])
+			if (format[curr_n] == FLAGS_CH[m])
 			{
 				flags |= FLAGS_ARR[m];
 				break;
@@ -30,7 +29,7 @@ int get_flags(const char *format, int *n)
 			break;
 	}
 
-	*n = cur_n - 1;
+	*n = curr_n - 1;
 
 	return (flags);
 }
